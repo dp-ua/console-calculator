d611954f733c3b81cc295353fe6c81531a100404
Pavel Reshetilo <pavel.reshetilo@sysgears.com>
2018-08-03 16:02:56 +0300

    Корректировка доков

--- src/main/java/user/UserInterface.java
+++ src/main/java/user/UserInterface.java
@@ -14,11 +14,11 @@ public class UserInterface {
      * Интерфейс, куда пользователь вводит данные
      * и откуда получает наши сообщения
      */
-    ConsoleHelper console;
+    private final ConsoleHelper console;
 
     /**
      * Базовый конструктор
-     * @param console
+     * @param console через что мы общаемся с пользователем
      */
     public UserInterface(ConsoleHelper console) {
         this.console = console;
@@ -26,31 +26,31 @@ public class UserInterface {
 
     /**
      * отправка сообщения пользователю
-     * @param message
+     * @param sInput  сообщение для пользователя
      */
-    public void send(String message) {
-        console.write(message);
+    public void send(String sInput) {
+        console.write(sInput);
     }
 
-    /**
-     * отправка отладочных сообщений
-     *
-     * @param message
-     * @param debugMode
-     */
-    public void send(String message, boolean debugMode) {
-        if (debugMode) console.write(":::::  " + message);
-    }
+// --Commented out by Inspection START (03.08.18 16:01):
+//    /**
+//     * отправка отладочных сообщений
+//     *
+//     * @param message
+//     * @param debugMode
+//     */
+//    public void send(String message, boolean debugMode) {
+//        if (debugMode) console.write(":::::  " + message);
+//    }
+// --Commented out by Inspection STOP (03.08.18 16:01)
 
     /**
      * Получение данных от пользователя
      *
-     * @return
-     * @throws IOException
+     * @return данные от пользователя
+     * @throws IOException ошибка при чтении
      */
     public String get() throws IOException {
-        String result = "";
-        result = console.read();
-        return result;
+        return console.read();
     }
 }
